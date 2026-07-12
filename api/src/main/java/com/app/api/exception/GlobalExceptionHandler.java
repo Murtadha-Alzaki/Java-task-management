9ac21cd19961a7472d5ae89829f10b57ac03a81b
Murtadha Alzaki <alzaki121212@gmail.com>
2026-07-12 22:27:01 +0300

    Add exception handler

---
+++ api/src/main/java/com/app/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.app.api.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Something went wrong", LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
